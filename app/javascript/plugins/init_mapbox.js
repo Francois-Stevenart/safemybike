4c0994c8f57e1575fafb09fdf8d8df89acf76b1e
Edouard De Prez <deprezedouard@gmail.com>
2019-08-20 12:53:31 +0200

    map first version complete

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,48 +1,52 @@
 import mapboxgl from 'mapbox-gl';
-
-const mapElement = document.getElementById('map');
-
-const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
-  });
-};
-
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
-    const element = document.createElement('div');
-    element.className = 'marker';
-    element.style.background = `white url('${marker.image_url}')`;
-    element.style.backgroundSize = 'cover';
-    element.style.borderRadius = '20px';
-    element.style.padding = '20px 0';
-    element.style.width = '40px';
-    element.style.height = '20px';
-
-    // Pass the element as an argument to the new marker
-    new mapboxgl.Marker(element)
-      .setLngLat([marker.lng, marker.lat])
-      .setPopup(popup)
-      .addTo(map);
-  });
-};
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+  // Create a HTML element for your custom marker
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.background = `white url('${marker.image_url}')`;
+    element.style.backgroundSize = '85%';
+    element.style.backgroundPosition= 'center';
+    element.style.backgroundRepeat = 'no-repeat';
+    element.style.borderRadius = '8px 8px 0 0';
+    element.style.padding = '10px 0';
+    element.style.width = '40px';
+    element.style.height = '30px';
+
+    new mapboxgl.Marker(element)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+};
+
+
 const initMapbox = () => {
-  if (mapElement) {
-    const map = buildMap();
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/deprezedouard/cjzb94xsi091p1co0m2e7j0uo'
+    });
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }));
   }
 };
 
