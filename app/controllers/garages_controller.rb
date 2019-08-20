427ab9c3b2ebfd812ab421bcab01a9d6672a7d83
Edouard De Prez <deprezedouard@gmail.com>
2019-08-20 11:23:31 +0200

    basic map functionality with markers

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,5 +1,13 @@
 class GaragesController < ApplicationController
   def index
+    @garages = Garage.geocoded #returns garages with coordinates
+
+    @markers = @garages.map do |garage|
+      {
+        lat: garage.latitude,
+        lng: garage.longitude
+      }
+    end
   end
 
   def show
