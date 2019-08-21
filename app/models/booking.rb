4b1443b2592d1e535fd4ba9b01a822e8cdedc41f
Edouard De Prez <deprezedouard@gmail.com>
2019-08-21 14:24:32 +0200

    more seed stuff

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :garage
   has_many :chats, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates :start_date, presence: true
   validates :status, inclusion: { in: %w(pending rejected accepted paid active done) }
