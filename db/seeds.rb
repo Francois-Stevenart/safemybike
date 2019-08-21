8128d2d48ba68fcdc785a2543bdfd3fada4cd3d0
Edouard De Prez <deprezedouard@gmail.com>
2019-08-21 13:58:40 +0200

    updated seed with more images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'faker'
 
 user_images = [
+  'https://nbocdn.akamaized.net/Assets/Images_Upload/2011/10/06/21-25810-actualite-apple-steve-jobs.jpg',
   'https://avatars0.githubusercontent.com/u/52085295?v=4',
   'https://avatars0.githubusercontent.com/u/51639218?v=4',
   'https://avatars2.githubusercontent.com/u/51084422?v=4',
@@ -9,8 +10,7 @@ user_images = [
   'https://avatars2.githubusercontent.com/u/51087021?v=4',
   'https://avatars2.githubusercontent.com/u/52207783?v=4',
   'https://avatars0.githubusercontent.com/u/50140032?v=4',
-  'https://avatars2.githubusercontent.com/u/43496861?v=4',
-  'https://nbocdn.akamaized.net/Assets/Images_Upload/2011/10/06/21-25810-actualite-apple-steve-jobs.jpg'
+  'https://avatars2.githubusercontent.com/u/43496861?v=4'
 ]
 
 garage_images = [
@@ -42,7 +42,6 @@ bike_images = [
   'https://res.cloudinary.com/di1eyazrv/image/upload/v1566382648/17-05-23-Nordhavn-DSC_8579_vuiuwm.jpg'
 ]
 
-
 puts 'Deleting all database content...'
 
 User.destroy_all
@@ -55,7 +54,7 @@ puts '-'*40
 puts 'Creating master user...'
 
 master_user = User.new(first_name: 'Federico', last_name: 'Pooface', email: 'master@user.com', password: 'master', password_confirmation: 'master')
-master_user.remote_profile_image_url = user_images.sample
+master_user.remote_profile_image_url = user_images[0]
 master_user.save!
 puts 'email: master@user.com'
 puts 'password: master'
@@ -65,7 +64,7 @@ puts '-'*40
 puts 'Creating owner user...'
 
 owner_user = User.new(first_name: 'Franky', last_name: 'Doodoo', email: 'owner@user.com', password: 'master', password_confirmation: 'master')
-owner_user.remote_profile_image_url = user_images.sample
+owner_user.remote_profile_image_url = user_images[1]
 owner_user.save!
 
 puts 'email: owner@user.com'
@@ -76,16 +75,31 @@ puts '-'*40
 puts 'Creating renter user...'
 
 renter_user = User.new(first_name: 'Eddy', last_name: 'King', email: 'renter@user.com', password: 'master', password_confirmation: 'master')
-renter_user.remote_profile_image_url = user_images.sample
+renter_user.remote_profile_image_url = user_images[2]
 renter_user.save!
 puts 'email: renter@user.com'
 puts 'password: master'
 
 puts '-'*40
 
-puts 'The master user goes out and buys a series of expensive bikes...'
+puts 'Creating 7 more users...'
+
+i = 3
+7.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "renter#{i}@user.com",
+    password: 'master',
+    password_confirmation: 'master')
+  user.remote_profile_image_url = user_images[i]
+  user.save!
+  i += 1
+end
 
 bike_names = %w(EddyMerckx-bike Cowboy Cowgirl Tandem HippyBike OldAndUgly)
+
+puts 'The master user goes out and buys a series of expensive bikes...'
 
 i = 0
 6.times do
@@ -110,6 +124,25 @@ bike = Bike.new(name: 'Cargo Bike', bike_size: 'cargo', bike_type: 'non-electric
 bike.user = renter_user
 bike.remote_bike_image_url = bike_images.sample
 bike.save!
+
+puts 'The other 7 users all go out and buy a bike.'
+
+bike_names = %w(TwoWheeler MasterCylinder TomBoonenBike EvenepoelBike BenoitPoocycle 6-wheeled-monster CarcoFishBike)
+
+users = User.all
+
+i = 0
+users.last(7).each do |user|
+  bike = Bike.new(
+    name: bike_names[i],
+    bike_size: ['regular', 'cargo'].sample,
+    bike_type: ['electric', 'non-electric'].sample
+    )
+  bike.user = user
+  bike.remote_bike_image_url = bike_images.sample
+  bike.save!
+  i += 1
+end
 
 puts 'Creating garage feature list...'
 
@@ -166,12 +199,14 @@ garages = master_user.garages
 bikes = Bike.all
 statuses = %w(pending rejected accepted paid active done)
 
-statuses.each do |status|
-  date = Faker::Date.between(from: Date.today, to: 1.month.from_now)
-  booking = Booking.new(start_date: date, status: status)
-  booking.garage = garages.sample
-  booking.bike = bikes.sample
-  booking.save!
+garages.each do |garage|
+  4.times do
+    date = Faker::Date.between(from: Date.today, to: 1.month.from_now)
+    booking = Booking.new(start_date: date, status: statuses.sample)
+    booking.garage = garage
+    booking.bike = bikes.sample
+    booking.save!
+  end
 end
 
 puts 'Success! Database seeded!'
