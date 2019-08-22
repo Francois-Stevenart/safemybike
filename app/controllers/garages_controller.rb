df9d4c599682deee6154b143c59eecc24a8859f9
Edouard De Prez <deprezedouard@gmail.com>
2019-08-22 17:03:16 +0200

    more stuff

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -9,9 +9,19 @@ class GaragesController < ApplicationController
       {
         lat: garage.latitude,
         lng: garage.longitude,
+        id: garage.id,
+        user_id: garage.user_id,
         infoWindow: render_to_string(partial: "info_window", locals: { garage: garage }),
         image_url: helpers.asset_url('/map_marker.png')
       }
+    end
+  end
+
+  def show_card
+    @garage = Garage.find(params[:id])
+    respond_to do |format|
+      p format
+      format.js
     end
   end
 
@@ -55,4 +65,5 @@ class GaragesController < ApplicationController
                                     :profile_image
                                   )
   end
+
 end
