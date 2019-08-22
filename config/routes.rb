cd5fc3175baeef2c08dc995ea5fa14f6a5a7d416
Francois-Stevenart <f.stevenart@gmail.com>
2019-08-22 16:28:30 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:show, :create] do
     resources :payments, only: [:new, :create]
   end
+  mount ActionCable.server => "/cable"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
