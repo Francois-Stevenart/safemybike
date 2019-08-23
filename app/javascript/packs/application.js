ffc16b13d19bc6e608e3c6e14d1bfddf5d28e775
Federico De Martino <federicodm90@gmail.com>
2019-08-23 12:27:22 +0200

    new calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-
+import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
