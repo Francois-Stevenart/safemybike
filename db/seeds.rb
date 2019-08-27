e4152ba256c64b7a146b02643d28251230fb015a
Edouard De Prez <deprezedouard@gmail.com>
2019-08-27 10:32:00 +0200

    adjusted seed, some other little stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -265,7 +265,7 @@ puts ''
 
 puts "Pulling reviews out of my buttocks... ⌨️"
 
-bookings = Booking.where.not(garage_id: master_user.garage.id)
+bookings = Booking.where.not(garage_id: master_user.garages.map { |garage| garage.id })
 
 bookings.each do |booking|
   if booking.status == 'done'
