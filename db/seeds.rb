d10494c2a6f63ec56e4dba6742f0a7e16c0bb1ce
Federico De Martino <federicodm90@gmail.com>
2019-08-30 13:42:36 +0200

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -184,9 +184,9 @@ puts ''
 puts 'Creating garage feature list... ⛽'
 
 Feature.new(name: 'bicycle rack', icon_image: '<i class="fas fa-stream"></i>').save!
+Feature.new(name: 'tire pump', icon_image: '<i class="fas fa-wind"></i>').save!
 Feature.new(name: 'charging station', icon_image: '<i class="fas fa-charging-station"></i>').save!
 Feature.new(name: 'electric tire pump', icon_image: '<i class="fas fa-bolt"></i>').save!
-Feature.new(name: 'tire pump', icon_image: '<i class="fas fa-wind"></i>').save!
 Feature.new(name: 'pin-code', icon_image: '<i class="fas fa-lock"></i>').save!
 Feature.new(name: 'camera security', icon_image: '<i class="fas fa-video"></i>').save!
 
